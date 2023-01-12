ebbb8889daa8f94be9dd406ad67a572d2bf584f8
TuvaStange <102515232+TuvaStange@users.noreply.github.com>
2023-01-12 14:37:11 +0100

    yas

---
+++ kode/Carousel/carousel.tsx
@@ -0,0 +1,8 @@
+import
+    "react-responsive-carousel/carousel.min.css";
+
+import { Carousel as ReactCarousel }
+    from "react-responsive-carousel";
+
+import "./style.css";
+import { FC, useState } from "react";
