f1a55020c297e0f4582213160f6045ad93adb78e
TuvaStange <tuvsta13@o365.mrvgs.no>
2023-01-25 21:15:36 +0100

    ferdigstille

--- kode/Carousel/carousel.tsx
+++
@@ -1,8 +0,0 @@
-import
-    "react-responsive-carousel/carousel.min.css";
-
-import { Carousel as ReactCarousel }
-    from "react-responsive-carousel";
-
-import "./style.css";
-import { FC, useState } from "react";
