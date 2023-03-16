81400cda6ceb8cbcefda5fed3202c906828aa8d0
TuvaStange <tuvsta13@o365.mrvgs.no>
2023-03-16 15:59:25 +0100

    jippi

--- kode/script.js
+++ kode/script.js
@@ -20,25 +20,8 @@ const rightArrow = document.querySelector('.right-arrow');
 
 let index = 0;
 
-images.forEach((item, i) => {
-  item.addEventListener('click', () => {
-    updateImage(i);
-    popup.classList.toggle('popup');
-    console.log("hei");
-  })
-})
-
-const updateImage = (i) => {
-  let path = 'img/img${i+1}.png'
-  largeImage.src = path;
-  imageName.innerHTML = path;
-  imageIndex.innerHTML = '0${i+1}';
-  index = i;
-  
-}
 
 closeBtn.addEventListener('click', () => {
-  popup.classList.toggle('popup');
   document.getElementById("popup").style.display = "none";
 })
 
